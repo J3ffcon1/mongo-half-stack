f899abaeaa211c999dcd943dba9905d8a8473fa7
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-16 09:49:07 -0700

    setting up http server file

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () =>{
+    console.log('server is runnin on', server.address().port);
+});
