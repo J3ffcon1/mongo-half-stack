21f8bdafa06d339aab7d0710eb550bf2ccb237bf
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-16 10:47:45 -0700

    added connection string for Mongo server, returns error.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* eslint no-console:off*/ 
 const http = require('http');
 const app = require('./lib/app');
+require('./lib/mongodb');
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
